31bae39eb1ed0f35b0d575663fd7869cb744bd75
taf-touch <taf.touch@gmail.com>
2020-06-17 03:34:47 +0200

    added single site app in svelte

---
+++ site/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Link from './Link.svelte';
+	import Route from './Route.svelte';
+</script>
+
+<style>
+</style>
+
+<!-- Navigation goes here -->
+<Link page={{path: '/', name: 'Home'}} />
+<Link page={{path: '/about', name: 'About'}} />
+
+<!-- Main container goes here -->
+<Route />
