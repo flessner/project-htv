9c4e6011b76e6d3881b76da18513dc7107a8f7b8
taf-touch <taf.touch@gmail.com>
2020-06-26 12:42:42 +0200

    getting model from s3

--- api/v1/handler.js
+++ api/v1/handler.js
@@ -1,11 +1,35 @@
 AWS = require('aws-sdk');
 s3 = new AWS.S3();
 
+fs = require("fs");
 Response = require('smarch-lib-responses');
+
 tf = require('@tensorflow/tfjs');
 Jimp = require('jimp');
 
 module.exports.k1006 = async event => {
+  var modelName = 'model-k1006.tflite';
+
+  // Getting model from S3, if it doesn't exist
+  try {
+    if (fs.existsSync(path)) {
+      console.log('Getting model was skipped.')
+    }
+  } catch(e) {
+    try {
+      var tfModel = await s3.getObject({
+        Bucket: process.env.S3_MODELS,
+        Key: 'v1/' + modelName
+      }).promise();
+      fs.writeFileSync('/tmp/' + modelName, tfModel.Body.toString());
+      console.log('Model was saved under /tmp/' + modelName);
+    } catch (e) {
+        console.error(e);
+        return Response._500({"message": "The service wasn't able to acquire the model data."});
+    }
+  }
+
+  // Change working directory to /tmp
   try {
     process.chdir('/tmp');
     console.log('New working directory: ' + process.cwd());
@@ -14,6 +38,7 @@ module.exports.k1006 = async event => {
     return Response._500({"message": "Failed to change working directory!"});
   }
 
+  // Getting image from request body
   try {
     var base64 = JSON.stringify(JSON.parse(event.body).data.image);
     console.log('Recieved the base 64 image.');
@@ -22,22 +47,7 @@ module.exports.k1006 = async event => {
     return Response._400({"message": "The Image couldn't be located at body/data/image."});
   }
 
-  if (tfModel == undefined) {
-    try {
-      var tfModel = await s3.getObject({
-        Bucket: process.env.S3_MODELS,
-        Key: 'v1/model-k1006.tflite'
-      }).promise();
-      fs.writeFileSync('/tmp/model-k1006.tflite', tfModel.Body.toString());
-      console.log('Model was saved under /tmp directory.')
-    } catch (e) {
-        console.error(e);
-        return Response._500({"message": "The service wasn't able to acquire the model data."});
-    }
-  } else {
-    console.log('Getting model was skipped.')
-  }
-
+  // Resizing & Saving image
   try {
     Jimp.read(Buffer.from(base64.replace(/data:image\/(png|jpg|jpeg|gif);base64,/, ''), 'base64'))
     .then(image => {
@@ -51,7 +61,7 @@ module.exports.k1006 = async event => {
   }
 
   return Response._200({
-    "message": "Go Serverless!",
+    "message": "We did it!",
     "data": {}
   });
 }
