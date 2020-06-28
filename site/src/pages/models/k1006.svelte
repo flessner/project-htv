12f92a9c411ecaf8239553de8b754776221b3fbd
taf-touch <taf.touch@gmail.com>
2020-06-28 19:11:44 +0200

    added simple gui on k1006

--- site/src/pages/models/k1006.svelte
+++ site/src/pages/models/k1006.svelte
@@ -7,7 +7,7 @@
 
     function setFiles() {
         image_queue = [];
-        console.log('Triggered Queue update')
+        console.log('Triggered Queue update, files are:')
 
         async function readAndPreview(file) {
             var reader = new FileReader();
@@ -22,19 +22,39 @@
         }
         console.log(image_queue)
     }
+
+    function clearResults() {
+        console.log('Results cleared')
+    }
 </script>
 
 <NavBar />
 <LoginBar />
 
 <h1>K1006</h1>
-<div class=>
+<div class='file-actions'>
     <input  type='file' accept="image/jpeg, image/png" bind:files on:change={setFiles} multiple>
+    <button on:click={clearResults}><b>Clear</b></button>
 </div>
 
 <style>
 h1 {
     text-align: center;
     text-decoration: underline;
+}
+
+.file-actions {
+    justify-content: space-around;
+    align-items: center;
+    display: flex;
+}
+
+.file-actions button {
+    padding: 8px;
+    width: 120px;
+    text-align: center;
+    border-radius: 5px;
+    color: #FFF;
+    background: rgb(141, 2, 31);
 }
 </style>
